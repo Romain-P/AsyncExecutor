084674f776acd6872565475fe44b427c3e8db1fe
johny <jonhy@na.com>
2024-11-19 18:41:40 +0100

    feat: all :O

---
+++ AsyncExecutor.h
@@ -0,0 +1,113 @@
+#include <atomic>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <optional>
+
+template <typename ResultType>
+struct Task {
+    uint32_t id;
+    std::optional<ResultType> result;
+    std::exception_ptr exception;
+};
+
+template <>
+struct Task<void> {
+    uint32_t id;
+    std::exception_ptr exception;
+};
+
+class AsyncExecutor {
+public:
+    AsyncExecutor() : task_id_counter(0), finished_task_count(0), max_valid_task_id(0) {}
+
+    ~AsyncExecutor() {
+        cancel_all();
+    }
+
+    template <typename ResultType>
+    uint32_t execute(std::function<ResultType()> task) {
+        uint32_t task_id = task_id_counter++;
+        std::thread([this, task_id, task]() {
+            try {
+                auto result = std::make_shared<ResultType>(task());
+                mark_task_finished<ResultType>(task_id, result);
+            } catch (...) {
+                mark_task_exception(task_id, std::current_exception());
+            }
+        }).detach();
+        return task_id;
+    }
+
+    template <typename ResultType>
+    std::shared_ptr<Task<ResultType>> try_pop() {
+        if (finished_task_count.load(std::memory_order_relaxed) == 0) {
+            return nullptr;
+        }
+
+        if (!mutex_.try_lock()) {
+            return nullptr;
+        }
+
+        std::lock_guard<std::mutex> lock(mutex_, std::adopt_lock);
+
+        if (!finished_tasks.empty()) {
+            auto it = finished_tasks.begin();
+            auto base_task = std::move(it->second);
+            finished_tasks.erase(it);
+            finished_task_count.fetch_sub(1, std::memory_order_relaxed);
+
+            auto task = std::static_pointer_cast<Task<ResultType>>(base_task);
+            return task;
+        }
+
+        return nullptr;
+    }
+
+    void cancel(uint32_t id) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        cancelled_task_ids.insert(id);
+    }
+
+    void cancel_all() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        max_valid_task_id.store(task_id_counter.load(std::memory_order_relaxed), std::memory_order_relaxed);
+        cancelled_task_ids.clear();
+    }
+
+private:
+    template <typename ResultType>
+    void mark_task_finished(uint32_t task_id, std::shared_ptr<ResultType> result) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (task_id < max_valid_task_id.load(std::memory_order_relaxed) || cancelled_task_ids.count(task_id)) {
+            return;
+        }
+        auto task = std::make_shared<Task<ResultType>>();
+        task->id = task_id;
+        task->result = *result;
+        finished_tasks.emplace(task_id, task);
+        finished_task_count.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    void mark_task_exception(uint32_t task_id, std::exception_ptr eptr) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (task_id < max_valid_task_id.load(std::memory_order_relaxed) || cancelled_task_ids.count(task_id)) {
+            return;
+        }
+        auto task = std::make_shared<Task<void>>();
+        task->id = task_id;
+        task->exception = eptr;
+        finished_tasks.emplace(task_id, task);
+        finished_task_count.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    std::atomic<uint32_t> task_id_counter;
+    std::atomic<int> finished_task_count;
+    std::atomic<uint32_t> max_valid_task_id;
+    std::mutex mutex_;
+    std::unordered_map<uint32_t, std::shared_ptr<void>> finished_tasks;
+    std::unordered_set<uint32_t> cancelled_task_ids;
+};
---
+++ README.md
@@ -0,0 +1,48 @@
+#### Sample
+
+Problem / How to call blocking calls from a rendering thread without impacting fps  
+Solution: AsyncExecutor ftw
+
+```cpp
+#include "beauty/beauty.hpp" //https://github.com/dfleury2/beauty
+
+
+//simulate a game loop that requires http requests & get result
+int main() { 
+    AsyncExecutor async;
+
+	//let's assume the requests are sent from the while. didn't add it on purpose for benchmarking
+    for (int i=0; i < 1; ++i) {
+        async.execute<beauty::response>([] {
+            beauty::client client;
+
+            // Request an URL
+            auto [ec, response] = client.get("https://pastebin.com/raw/Q6n031Cn");
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            return response;
+        });
+    }
+
+	//simulate game thread
+    while(true) {
+        auto now = std::chrono::duration_cast<std::chrono::nanoseconds >(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        auto result = async.try_pop<beauty::response>();
+
+        if (result != nullptr) {
+            bool x = result->result->is_status_ok();
+            auto after = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+            auto diff = after - now;
+            printf("atomic read + lock acquire : time diff : %lld\n", diff);
+        }
+    }
+}
+```
+ 
+try_pop consumes ~500/1100 ns per frame, which is far decent. Also note that it's not required to poll task every frame, every 200ms (5 frames) sounds reasonnable
+
+##### TODO / LIMITATIONS
+
+Currently, cancel one specific or all tasks does not actually kill the thread, tasks will continue on their separate thread, but the results won't be pushed.  
+I did that on purpose: the external libs that's im using for my project are completely blocking without any option to cancel a task (I/O or crypto alg.).  
+
+If you actually need to stop the thread, you can give a shot to the risky TerminateThread api of windows, or add a new atomic integer `CancelRequested` and passes it to the task so it can cancel on demand (if you have the hand on it).
